c874ce63f26ab3e888bfb98d6299db99c1e32708
lang <15829391774@163.com>
2016-08-10 08:37:58 +0800

    trace 格式修改

--- trace.h
+++ trace.h
@@ -1,5 +1,5 @@
 #ifndef __NOTRACE
-#define Trace(...) printf("file[%s]func[%s]line[%d]##", __FILE__,__func__, __LINE__);printf(__VA_ARGS__);printf("\n");
+#define Trace(...) printf("file[%s]func[ %s ]line[ %d ]#", __FILE__,__func__, __LINE__);printf(__VA_ARGS__);printf("\n");
 
 #else
 #define Trace(...)
