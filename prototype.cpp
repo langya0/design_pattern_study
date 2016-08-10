cb00705a6094c3c0369675415ed2531e13b01161
lang <15829391774@163.com>
2016-08-10 11:38:48 +0800

    原型模式：支持赋值/拷贝的，不存在深浅拷贝等问题即可。所以在C+实现拷贝构造，赋值运算符即可。java没有这个，所以需要重载lone

---
+++ prototype.cpp
@@ -0,0 +1,43 @@
+//原型模式是内存拷贝的。所以，只需要实现赋值，不存在深浅拷贝带来的问题
+// 那就可以称之为原型模式。在C++中，也就是重载了质变拷贝构造，赋值运算符就好
+
+
+#include "afxstd.h"
+
+//
+class concretePrototype{
+public:
+	concretePrototype()
+	{
+		Trace("");
+	}
+	concretePrototype(const	concretePrototype &con)
+	:_mm(con._mm)
+	{
+		Trace("");
+	}
+
+	concretePrototype& operator=(const concretePrototype &con)
+	{
+		if(this!=&con)
+		{
+			Trace("");
+			this->_mm = con._mm;
+		}
+		return *this;
+	}
+	~concretePrototype()
+	{
+		Trace("");
+	}
+protected:
+	string _mm;
+};
+int main(int argc, char const *argv[])
+{
+	concretePrototype x;
+	concretePrototype y(x);	
+	concretePrototype z = y;	
+	z = x;
+	return 0;
+}
